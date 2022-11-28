1557997a997aade56b4d5488081626aef599d4f2
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-11-27 21:22:07 -0600

    Added comma formatting for numbers in strings

--- src/interactions/rollMagicItemCost.js
+++ src/interactions/rollMagicItemCost.js
@@ -16,23 +16,23 @@ const rollMagicItemCost = (rarity) => {
 
     if (rarity === "common") {
         magicItemCost = (rollDice(6) + 1) * 10;
-        return `Common magic item cost: ${magicItemCost} gp`
+        return `Common magic item cost: ${magicItemCost.toLocaleString("en-US")} gp`
 
     } else if (rarity === "uncommon") {
         magicItemCost = rollDice(6) * 100;
-        return `Uncommon magic item cost: ${magicItemCost} gp`
+        return `Uncommon magic item cost: ${magicItemCost.toLocaleString("en-US")} gp`
 
     } else if (rarity === "rare") {
         magicItemCost = (rollDice(10) + rollDice(10)) * 1000;
-        return `Rare magic item cost: ${magicItemCost} gp`
+        return `Rare magic item cost: ${magicItemCost.toLocaleString("en-US")} gp`
 
     } else if (rarity === "very-rare") {
         magicItemCost = (rollDice(4) + 1) * 10000;
-        return `Very rare magic item cost: ${magicItemCost} gp`
+        return `Very rare magic item cost: ${magicItemCost.toLocaleString("en-US")} gp`
 
     } else if (rarity === "legendary") {
         magicItemCost = (rollDice(6) + rollDice(6)) * 25000;
-        return `Legendary magic item cost: ${magicItemCost} gp`
+        return `Legendary magic item cost: ${magicItemCost.toLocaleString("en-US")} gp`
 
     }
 }
