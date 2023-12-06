8fc665b23ac4060ca4910d6459703220498f96e9
Jamie Coleman <j.a.coleman1978@gmail.com>
2023-12-05 23:43:37 -0600

    Put upper and lower boundaries for total in rollStatSet

---
+++ src/helpers/rollStatSet.js
@@ -0,0 +1,40 @@
+import rollDice from "./rollDice.js";
+
+const rollStat = () => {
+    let total = 0;
+
+    while (total < 6) {
+        let d1 = rollDice(6);
+        let d2 = rollDice(6);
+        let d3 = rollDice(6);
+        let d4 = rollDice(6);
+
+        let lowest = Math.min(d1, d2, d3, d4);
+
+        total = d1 + d2 + d3 + d4 - lowest;
+    }
+
+    return total;
+};
+
+const rollStatSet = () => {
+    let total = 0;
+    let statSet = [];
+    let maxStat = 0;
+
+    while (total < 68 || total > 78 || maxStat < 15) {
+        statSet = [];
+
+        for (let i = 0; i < 6; i++) {
+            statSet.push(rollStat());
+        }
+        
+        total = statSet.reduce((a, b) => a + b);
+
+        maxStat = Math.max(...statSet);
+    }
+
+    return statSet;
+};
+
+export default rollStatSet;
--- src/interactions/rollNewStatSet.js
+++ src/interactions/rollNewStatSet.js
@@ -1,10 +1,12 @@
-import rollStat from "../helpers/rollStat.js";
+import rollStatSet from "../helpers/rollStatSet.js";
 
 /*
 Generates a new stat set for a character.
 */
 const rollNewStatSet= () => {
-    return `New stat set: ${rollStat()}, ${rollStat()}, ${rollStat()}, ${rollStat()}, ${rollStat()}, ${rollStat()}`;
+    const statSet = rollStatSet();
+
+    return `New stat set: ${statSet[0]}, ${statSet[1]}, ${statSet[2]}, ${statSet[3]}, ${statSet[4]}, ${statSet[5]}`;
 };
 
 export default rollNewStatSet;
